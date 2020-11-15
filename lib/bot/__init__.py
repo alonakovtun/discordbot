74676076cace187223fb4e6e4110dd151c433c2d
alonakovtun <alena.sergeevna0512@gmail.com>
2020-11-15 20:28:48 +0100

    Commit #2

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from discord import Intents
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
 
@@ -11,16 +13,16 @@ class Bot(BotBase):
 		self.guild = None
 		self.scheduler = AsyncIOScheduler()
 
-		super().__init__(command_prefex = PREFIX, owner_ids = OWNER_IDS)
+		super().__init__(command_prefex = PREFIX,owner_ids = OWNER_IDS)
 
 	def run(self, version):
 		self.VERSION = version
 
-		with open("./lib/bot/token", "r", encoding="utf-8") as tf:
+		with open("./lib/bot/token.0", "r", encoding="utf-8") as tf:
 			self.TOKEN = tf.read()
 
 		print("running bot...")
-		super().run(self.TOKEN, recconect = True)
+		super().run(self.TOKEN, reconnect = True)
 
 	async def on_connect(self):
 		print("bot connected")
@@ -31,8 +33,24 @@ class Bot(BotBase):
 	async def on_ready(self):
 		if not self.ready:
 			self.ready = True
-			self.guild = self.get_guild(776501953593212950)
 			print("bot ready")
+
+			channel = self.get_channel(776501954054193223)
+			await channel.send("Now online!")
+
+			embed = Embed(title = 'Now online!', 
+			description = "Bot is now online!", 
+			color = "0xFF0000",
+			timespan)
+			fields = [("Name", "Value", True),
+					  ("Another field", "This field is next to the other one.")
+					  ("A non-inline field", "This field will apear on it's own row.", False)]
+			
+			for name, value, inline in fields:
+				embed.add.tield(name = name, value = value, inline = inline)
+				embed.set_footer(text = "This is a footer!")
+			await channel.send(embed = embed)
+			
 		else:
 			print("bot recconected")
 
