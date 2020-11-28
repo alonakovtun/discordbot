af387a7e8818c33fe9fa7357ea761a8053e414d3
alonakovtun <alena.sergeevna0512@gmail.com>
2020-11-28 14:16:01 +0100

    Commit #4 change init and db

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -2,11 +2,14 @@ from asyncio import sleep
 from glob import glob
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
+
 from discord import Embed , File
 from datetime import datetime
 from discord.ext.commands import (CommandNotFound, BadArgument, MissingRequiredArgument,CommandOnCooldown)
 from apscheduler.triggers.cron import CronTrigger
 from discord.errors import HTTPException, Forbidden
+
+#from ..db import db
 
 
 PREFIX = "+"
@@ -19,6 +22,7 @@ class Bot(BotBase):
 		self.guild = None
 		self.scheduler = AsyncIOScheduler()
 
+		#db.autosave(self.scheduler)
 		super().__init__(command_prefix=PREFIX,owner_ids=OWNER_IDS)
 
 	def run(self, version):
@@ -36,20 +40,41 @@ class Bot(BotBase):
 	async def on_disconnect(self):
 		print("bot disconnected")
 
+	async def on_error(self, err, *args, **kwargs):
+		if err == "on_command_error":
+			await args[0].send("Something went wrong!")
+		
+		channel = self.get_channel(776501954054193223)  
+		await channel.send("An error occured!")
+		raise
+
+	async def on_command_error(self, ctx, exc):
+		if isinstanse(exc, CommandNotFound):
+			pass
+
+		elif hasattr(exc, "original"):
+			raise exc.original
+		else:
+			raise exc
+
 	async def on_ready(self):
 		if not self.ready:
 			self.ready = True
+			self.guild = self.get_guild(776501953593212950)
+			self.scheduler.start()
 
-			print("bot ready")
 
-			channel = self.get_channel(776501954054193223)
+			channel = self.get_channel(776501954054193223)  
 			await channel.send("Now online!")
 
 			embed = Embed(title="Im here", description="Bot is online",colour=0xFF0000, timestamp=datetime.utcnow())
 			fields=[("Vladyslav Petriuk , Alona Kovtun , Vladyslava Tokar","Authors",True),("     w60083                     w60065                 w60092","Indexs",False),("Commands","----------------------------------------------------------------------------------",False),("Hello","Say +hi",True),("echo","+echo message",True),("punch","+punch member reason",True)]
 			for name ,value,inline in fields:
 				embed.add_field(name=name,value=value,inline=inline)
+			embed.set_author(name="Bot", icon_url=self.guild.icon_url)
 			await channel.send(embed=embed)
+
+			print("bot ready")
 		else:
 			print("bot recconected")
 
--- lib/db/db.py
+++ lib/db/db.py
@@ -1,6 +1,8 @@
 from os.path import isfile
 from sqlite import connect
 
+#from apscheduler.triggers.cron import CronTrigger
+
 DB_PATH = "./data/db/database.db"
 BUILD_PATH = "./data/db/build.sql"
 
@@ -22,34 +24,37 @@ def build():
 def commit():
 	cxn.commit()
 
+#def autosave(sched):
+	#sched.add_job(commit, CronTrigger(second=0))
+
 def close():
 	cxn.close()
 
-def field(command, *values)
+def field(command, *values):
 	cur.execute(command, tuple(values))
 
 	if(fetch != cur.fetchone()) is not None:
 		return fetch[0]
 
-def record(command, *values)
+def record(command, *values):
 	cur.execute(command, tuple(values))
 
 	return cur.fetchone()
 
-def records(command, *values)
+def records(command, *values):
 	cur.execute(command, tuple(values))
 
 	return cur.fetchall()
 
-def column(command, *values)
+def column(command, *values):
 	cur.execute(command, tuple(values))
 
 	return [item[0] for item in cur.fetchall()]
 
-def execute(command, *values)
+def execute(command, *values):
 	cur.execute(command, tuple(values))
 
-def multiexec(command, valueset)
+def multiexec(command, valueset):
 	cur.executemany(command, valueset)
 
 def scriptexec(path):
